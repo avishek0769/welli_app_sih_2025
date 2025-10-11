20a36c270a53e4e97df5c3a7e2387da0dc3f42af
avishek0769 <avishekadhikary42@gmail.com>
2025-10-11 22:38:29 +0530

    Create the basic structure of app.js for middlewares and all other routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routers/user.route.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 dotenv.config({
     path: "./.env",
@@ -8,9 +11,20 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+app.use(json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true
+}));
+
+// API routes
+app.use("/api/v1/user", userRouter);
+
+
+// Error handling middleware - must be last
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
