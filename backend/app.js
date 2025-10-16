a594ba9c45274635ae97ec64d7079c4da34f943a
avishek0769 <avishekadhikary42@gmail.com>
2025-10-16 23:41:27 +0530

    Installed and initialised a socket.io + http server

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+import http from "http"
 import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routers/user.route.js";
@@ -6,21 +7,27 @@ import errorHandler from "./middlewares/errorHandler.js";
 import forumRouter from "./routers/forum.route.js";
 import postRouter from "./routers/post.route.js";
 import commentRouter from "./routers/comment.route.js";
+import peerChatRouter from "./routers/peerchat.route.js";
+import likeRouter from "./routers/like.route.js";
+import { Server } from "socket.io";
+import { handleSendMessage } from "./controllers/peermessage.controller.js";
 
 dotenv.config({
     path: "./.env",
 });
 
 const app = express();
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN || "*",
+        credentials: true
+    }
+})
 const PORT = process.env.PORT || 3000;
 
 app.use(json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*",
-    credentials: true
-}));
 
 // API routes
 app.use("/api/v1/user", userRouter);
@@ -31,9 +38,17 @@ app.use("/api/v1/post", postRouter);
 
 app.use("/api/v1/comment", commentRouter);
 
+app.use("/api/v1/like", likeRouter);
+
+app.use("/api/v1/peerChat", peerChatRouter);
+
+io.on("connection", (socket) => {
+    socket.on("sendMessage", handleSendMessage(socket))
+})
+
 // Error handling middleware - must be last
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
