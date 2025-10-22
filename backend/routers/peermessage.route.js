79c19529589c1176572425cc7bd6b76069d6bda1
avishek0769 <avishekadhikary42@gmail.com>
2025-10-22 22:52:49 +0530

    Create the necessary routes for the peermessages

--- backend/routers/peermessage.route.js
+++ backend/routers/peermessage.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import auth from "../middlewares/auth"
-import { clearChat, deleteForMe, getMessagesByChat } from "../controllers/peermessage.controller"
+import { clearChat, deleteForEveryone, deleteForMe, getMessagesByChat, getUnreadMessageCountByChat } from "../controllers/peermessage.controller"
 
 const peermessageRouter = Router()
 
-peermessageRouter.route("/deleteForMe/:messageId").delete(auth, deleteForMe)
-peermessageRouter.route("/clear/:chatId").delete(auth, clearChat)
 peermessageRouter.route("/get/:chatId").get(auth, getMessagesByChat)
-peermessageRouter.route("/getAllUnread").get(auth, getMessagesByChat)
+peermessageRouter.route("/getAllUnread").get(auth, getUnreadMessageCountByChat)
+peermessageRouter.route("/deleteForMe/:messageId").delete(auth, deleteForMe)
+peermessageRouter.route("/deleteForEveryone/:messageId").delete(auth, deleteForEveryone)
+peermessageRouter.route("/clear/:chatId").delete(auth, clearChat)
 
 export default peermessageRouter
