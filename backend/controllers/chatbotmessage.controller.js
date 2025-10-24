b15d131b78937d2e681fc06817c78e50af4b29a9
avishek0769 <avishekadhikary42@gmail.com>
2025-10-25 01:16:29 +0530

    Create the controllers for chatbot messages leaving the python service logic

---
+++ backend/controllers/chatbotmessage.controller.js
@@ -0,0 +1,78 @@
+import ChatbotConversation from "../models/chatbotcoversation.model"
+import ChatbotMessage from "../models/chatbotmessage.model"
+import ApiError from "../utils/ApiError"
+import ApiResponse from "../utils/ApiResponse"
+
+const createChatbotMessage = asyncHandler(async (req, res) => {
+    const { userInput, chatId } = req.body
+
+    const chat = await ChatbotConversation.findById(chatId)
+    if(chat.user != req.user._id) {
+        throw new ApiError(402, "This chat does not belong to this user")
+    }
+
+    // const response = await fetch("/api/python-service", {
+    //     method: "POST",
+    //     body: JSON.stringify({ userInput })
+    // })
+    // const data = await response.json()
+    const data = { chatBotResponse: "New Chat" }
+
+    await ChatbotMessage.create({
+        chat: chatId,
+        promptText: userInput,
+        responseText: data.chatBotResponse,      
+    })
+
+    return res.status(200).json(new ApiResponse(200, data, "Chatbot response fetched"))
+})
+
+const getAllChats = asyncHandler(async (req, res) => {
+    const { chatId } = req.params;
+    const { page = 0, limit = 10 } = req.query
+
+    const chat = await ChatbotConversation.findById(chatId)
+    if(chat.user != req.user._id) {
+        throw new ApiError(402, "This chat does not belong to this user")
+    }
+
+    const messages = await ChatbotMessage
+        .find({ chat: chatId })
+        .skip(Number(page) * Number(limit))
+        .limit(Number(limit))
+        .sort({ timestamp: -1 })
+
+    return res.status(200).json(new ApiResponse(200, messages, "Fetched all chatbot messages for this conversation"))
+})
+
+const getAudio = asyncHandler(async (req, res) => {
+    const { messageId } = req.params;
+
+    const message = await ChatbotMessage.findById(messageId)
+    const chat = await ChatbotConversation.findById(message.chat)
+    if(chat.user != req.user._id) {
+        throw new ApiError(402, "This message does not belong to this user")
+    }
+
+    if(message.responseAudioUrl) {
+        return res.status(200).json(new ApiResponse(200, { audioUrl: message.responseAudioUrl }, "Fetched audio for the chatbot message"))
+    }
+
+    // const response = await fetch("/api/python-tts-service", {
+    //     method: "POST",
+    //     body: JSON.stringify({ text: message.responseText })
+    // })
+    // const data = await response.json()
+    const data = { audioUrl: "http://example.com/audio.mp3" }
+
+    message.responseAudioUrl = data.audioUrl
+    await message.save()
+
+    return res.status(200).json(new ApiResponse(200, data, "Fetched audio for the chatbot message"))
+})
+
+export {
+    createChatbotMessage,
+    getAllChats,
+    getAudio
+}
